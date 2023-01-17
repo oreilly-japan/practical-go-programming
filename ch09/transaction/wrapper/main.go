a5d8239d7813b5eb988bc10adce5fd859ad18457
Akihiro Takizawa <atakizawa@oreilly.co.jp>
2023-01-17 15:20:34 +0900

    #24 の問題を修正

--- ch09/transaction/wrapper/main.go
+++ ch09/transaction/wrapper/main.go
@@ -13,34 +13,34 @@ type txAdmin struct {
 }
 
 type Service struct {
-	tx txAdmin
+	txAdmin
 }
 
 // Transaction はトランザクションを制御するメソッド
 // アプリケーション開発者が本メソッドを使って、DMLのクエリーを発行する
-func (t *txAdmin) Transaction(ctx context.Context, f func(ctx context.Context) (err error)) error {
+func (t *txAdmin) Transaction(ctx context.Context, f func(ctx context.Context, tx *sql.Tx) (err error)) error {
 	tx, err := t.BeginTx(ctx, nil)
 	if err != nil {
 		return err
 	}
 	defer tx.Rollback()
-	if err := f(ctx); err != nil {
+	if err := f(ctx, tx); err != nil {
 		return fmt.Errorf("transaction query failed: %w", err)
 	}
 	return tx.Commit()
 }
 
 func (s *Service) UpdateProduct(ctx context.Context, productID string) error {
-	updateFunc := func(ctx context.Context) error {
-		if _, err := s.tx.ExecContext(ctx, "..."); err != nil {
+	updateFunc := func(ctx context.Context, tx *sql.Tx) error {
+		if _, err := tx.ExecContext(ctx, "..."); err != nil {
 			return err
 		}
-		if _, err := s.tx.ExecContext(ctx, "..."); err != nil {
+		if _, err := tx.ExecContext(ctx, "..."); err != nil {
 			return err
 		}
 		return nil
 	}
-	return s.tx.Transaction(ctx, updateFunc)
+	return s.Transaction(ctx, updateFunc)
 }
 
 // transaction-wrapper-end
